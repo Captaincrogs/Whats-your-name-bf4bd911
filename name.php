22c58aa985b1825d4806b99be967abde8e5c765e
Captaincrogs <2003829@talnet.nl>
2019-10-16 14:48:57 +0200

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,6 @@
+<?php
+echo "Gegroet!", PHP_EOL, PHP_EOL;
+$voornaam = readline("Wat is je voornaam?: ");	
+$achternaam = readline("Wat is je achternaam?: ");
+echo "Jouw volledige naam is: $voornaam $achternaam";
+?>	
